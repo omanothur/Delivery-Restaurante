4e9cf91fe5cfce9fc6a891fc94d37a3686e30ecb
omanothur <arthuralc2017@gmail.com>
2024-09-08 23:01:00 -0300

    feat: returning and dealing with registration errors, involving filling out and existing accounts.

--- script/cadastro.js
+++ script/cadastro.js
@@ -1,38 +1,69 @@
-async function cadastrar(){ 
-    console.log("aaa")
-    const Url="https://go-wash-api.onrender.com/api/user"
-    let Nome = document.getElementById("Nome").value
-    let CPF_CNPJ = document.getElementById("CPF/CNPJ").value
-    let Data_de_nascimeento = document.getElementById("Data de nascimento").value
-    let Email = document.getElementById("Email").value
-    let Senha = document.getElementById("Senha").value
-    let Confirmar_Senha = document.getElementById("Confirmar Senha").value
-    let terms = document.getElementById("termos").checked
+async function cadastrar(){
+  console.log("aaa")
+  const Url = "https://go-wash-api.onrender.com/api/user"
+  let Nome = document.getElementById("name").value
+  let CPF_CNPJ = document.getElementById("cpf_cnpj").value
+  let Data_de_nascimento = document.getElementById("birthday").value
+  let Email = document.getElementById("email").value
+  let Senha = document.getElementById("password").value
+  let Confirmar_Senha = document.getElementById("confirm_password").value
+  let terms = document.getElementById("termos").checked
 
-    if (Senha !== Confirmar_Senha){
-        return alert("Senha invalida")
-    }
+  if(Nome === ""){
+    return alert("É necessário colocar seu nome completo!")
+  }
 
-    let dados ={
-    "name":Nome,
-    "email":Email,
-    "user_type_id":1,
+  if(CPF_CNPJ === ""){
+    return alert("É necessário colocar seu CPF ou CNPJ!")
+  }
+
+  if(Data_de_nascimento === ""){
+    return alert("É necessário colocar sua data de nascimento!")
+  }
+
+  if(Email === ""){
+    return alert("É necessário colocar seu email!")
+  }
+
+  if (Senha !== Confirmar_Senha || Confirmar_Senha === ""){
+    return alert("Senha invalida")
+  }
+
+  let dados = {
+    "name": Nome,
+    "email": Email,
+    "user_type_id": 1,
     "password": Senha,
     "cpf_cnpj": CPF_CNPJ,
-    "terms": terms == true? 1 : 0,
-    "birthday":Data_de_nascimeento
+    "terms": terms == true ? 1 : 0,
+    "birthday": Data_de_nascimento
+  }
+
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  const requisicao = await fetch(Url, {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify(dados),
+  });
+
+  if(requisicao.ok){
+    const resposta = await requisicao.json();
+    alert(resposta.data)
+  }else{
+    if(requisicao.status===500){
+    return alert("Erro ao realizar seu cadastro! Tente novamente mais tarde.")
     }
-    
-    const headers = {
-        'Content-Type': 'application/json',
-      };
+    const resposta = await requisicao.json();
 
-      const requisicao = await fetch(Url, {
-        method: 'POST',
-        headers: headers,
-        body: JSON.stringify(dados),
-      });
+    if('email' in resposta.data.errors){
+      return alert(resposta.data.errors.email)
+    }
 
-      const resposta = await requisicao.json();
-      console.log(resposta);
+    if('cpf_cnpj' in resposta.data.errors){
+      return alert(resposta.data.errors.cpf_cnpj)
+    }
+  }
 }
