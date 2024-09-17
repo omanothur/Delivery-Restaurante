91cc50b734d9a822580215914a5bd6eab4b93223
omanothur <arthuralc2017@gmail.com>
2024-09-17 13:36:27 -0300

    feat: error for matheus to check

--- script/cadastro.js
+++ script/cadastro.js
@@ -1,3 +1,105 @@
+function resetFieldsErrors(
+  nameError,
+  cpf_cnpjError,
+  birthdayError,
+  emailError,
+  passwordError,
+  confirm_passwordError
+) {
+  nameError.classList.remove('field-error-active');
+  nameError.innerHTML = '';
+
+  cpf_cnpjError.classList.remove('field-error-active');
+  cpf_cnpjError.innerHTML = '';
+
+  birthdayError.classList.remove('field-error-active');
+  birthdayError.innerHTML = '';
+
+  emailError.classList.remove('field-error-active');
+  emailError.innerHTML = '';
+
+  passwordError.classList.remove('field-error-active');
+  passwordError.innerHTML = '';
+
+  confirm_passwordError.classList.remove('field-error-active');
+  confirm_passwordError.innerHTML = '';
+}
+
+function checkFields(
+  name,
+  cpf_cnpj,
+  birthday,
+  email,
+  password,
+  confirm_password
+) {
+  let nameError = document.getElementById('nameError');
+  let cpf_cnpjError = document.getElementById('cpf_cnpjError');
+  let birthdayError = document.getElementById('birthdayError');
+  let emailError = document.getElementById('emailError');
+  let passwordError = document.getElementById('passwordError');
+  let confirm_passwordError = document.getElementById('confirm_passwordError');
+}
+
+resetFieldsErrors(
+  nameError,
+  cpf_cnpjError,
+  birthdayError,
+  emailError,
+  passwordError,
+  confirm_passwordError
+);
+
+let hasError = false;
+
+
+if(Nome.value === ""){
+  hasError = true;
+  nameError.innerHTML = 'Preencha o campo nome';
+  nameError.classList.add('field-error-active')
+}
+
+if(CPF_CNPJ.value === ""){
+  hasError = true;
+  nameError.innerHTML = 'Preencha o campo cpf/cnpj'
+  nameError.classList.add('field-error-active')
+}
+
+if(Data_de_nascimento.value === ""){
+  hasError = true;
+  nameError.innerHTML = 'Preencha o campo data de nascimento'
+  nameError.classList.add('field-error-active')
+}
+
+if(Email.value === ""){
+  hasError = true;
+  nameError.innerHTML = 'Preencha o campo e-mail'
+  nameError.classList.add('field-error-active')
+}
+
+if(password.value === ''){
+  hasError = true;
+  nameError.innerHTML = 'Preencha o campo senha'
+  nameError.classList.add('field-error-active')
+}
+
+if (confirm_password.value === '') {
+  hasError = true;
+  confirm_passwordError.innerHTML = 'Preencha o campo confirmar senha';
+  confirm_passwordError.classList.add('field-error-active');
+
+if(
+  passowrd !== '' &&
+  confirm_password.value !== '' &&
+  confirm_password.value !== password.value
+){
+  hasError = true;
+  confirm_passwordError.innerHTML = 'Senha diferentes';
+  confirm_passwordError.classList.add('field-error-active')
+}
+return {hasError};
+}
+
 async function cadastrar(){
   const Url = "https://go-wash-api.onrender.com/api/user"
   let Nome = document.getElementById("name").value
@@ -7,26 +109,6 @@ async function cadastrar(){
   let Senha = document.getElementById("password").value
   let Confirmar_Senha = document.getElementById("confirm_password").value
   let terms = document.getElementById("termos").checked
-
-  if(Nome === ""){
-    return alert("É necessário colocar seu nome completo!")
-  }
-
-  if(CPF_CNPJ === ""){
-    return alert("É necessário colocar seu CPF ou CNPJ!")
-  }
-
-  if(Data_de_nascimento === ""){
-    return alert("É necessário colocar sua data de nascimento!")
-  }
-
-  if(Email === ""){
-    return alert("É necessário colocar seu email!")
-  }
-
-  if (Senha !== Confirmar_Senha || Confirmar_Senha === ""){
-    return alert("Senha invalida")
-  }
 
   let dados = {
     "name": Nome,
