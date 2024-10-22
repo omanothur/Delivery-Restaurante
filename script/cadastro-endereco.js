a1ed53ea8dd5386ebffc8d3239b43bec5099d1e3
Matheus Abreu <matheusabreutech@gmail.com>
2024-10-22 19:31:28 -0300

    feat: redirect to list of address

--- script/cadastro-endereco.js
+++ script/cadastro-endereco.js
@@ -1,22 +1,15 @@
-function verification_login(){
-  if('token' in localStorage){
-      let status = true
-      const form = document.getElementById('myForm');
-      form.classList.add('show');
-    }else{
-      let status = false
-      window.location.href = "./login.html"
+function verification_login() {
+  if ('token' in localStorage) {
+    let status = true;
+    const form = document.getElementById('myForm');
+    form.classList.add('show');
+  } else {
+    let status = false;
+    window.location.href = './login.html';
   }
 }
 
-
-function resetFieldsErrors(
-  cepError,
-  enderecoError,
-  numeroError,
-  nameError
-) {
-
+function resetFieldsErrors(cepError, enderecoError, numeroError, nameError) {
   cepError.classList.remove('field-error-active');
   cepError.innerHTML = '';
 
@@ -28,94 +21,74 @@ function resetFieldsErrors(
 
   nameError.classList.remove('field-error-active');
   nameError.innerHTML = '';
-
 }
 
-function checkFields(
-  name,
-  cep,
-  endereco,
-  numero
-) {
+function checkFields(name, cep, endereco, numero) {
   let cepError = document.getElementById('cepError');
   let enderecoError = document.getElementById('enderecoError');
   let numeroError = document.getElementById('numeroError');
   let nameError = document.getElementById('nameError');
   let submitButton = document.getElementById('submitButton');
 
-  resetFieldsErrors(
-    cepError,
-    enderecoError,
-    numeroError,
-    nameError
-  );
+  resetFieldsErrors(cepError, enderecoError, numeroError, nameError);
 
   let hasError = false;
 
-  if (name.value === "") {
+  if (name.value === '') {
     hasError = true;
     nameError.innerHTML = 'Preencha o campo nome';
-    nameError.classList.add('field-error-active')
+    nameError.classList.add('field-error-active');
   }
 
-  if (cep.value === "") {
+  if (cep.value === '') {
     hasError = true;
     cepError.innerHTML = 'Preencha o campo CEP';
-    cepError.classList.add('field-error-active')
+    cepError.classList.add('field-error-active');
   }
 
-  if (endereco.value === "") {
+  if (endereco.value === '') {
     hasError = true;
-    enderecoError.innerHTML = 'Preencha o campo endereço'
-    enderecoError.classList.add('field-error-active')
+    enderecoError.innerHTML = 'Preencha o campo endereço';
+    enderecoError.classList.add('field-error-active');
   }
 
-  if (numero.value === "") {
+  if (numero.value === '') {
     hasError = true;
-    numeroError.innerHTML = 'Preencha o campo número'
-    numeroError.classList.add('field-error-active')
+    numeroError.innerHTML = 'Preencha o campo número';
+    numeroError.classList.add('field-error-active');
   }
   return { hasError };
 }
 
-
 async function cadastrar_endereco() {
-  const Url = "https://go-wash-api.onrender.com/api/auth/address"
-  let Name = document.getElementById("name")
-  let Cep = document.getElementById("cep")
-  let Endereco = document.getElementById("endereco")
-  let Numero = document.getElementById("numero")
-  let Complemento = document.getElementById("complemento")
-  const fieldErrors = checkFields(
-    Name,
-    Cep,
-    Endereco,
-    Numero,
-    Complemento
-  )
-  
+  const Url = 'https://go-wash-api.onrender.com/api/auth/address';
+  let Name = document.getElementById('name');
+  let Cep = document.getElementById('cep');
+  let Endereco = document.getElementById('endereco');
+  let Numero = document.getElementById('numero');
+  let Complemento = document.getElementById('complemento');
+  const fieldErrors = checkFields(Name, Cep, Endereco, Numero, Complemento);
 
   if (fieldErrors.hasError) {
-    return
+    return;
   }
 
   let dados = {
-    "title": Name.value,
-    "cep": Cep.value,
-    "address": Endereco.value,
-    "number": Numero.value,
-    "complement": Complemento.value || undefined
-  }
-
+    title: Name.value,
+    cep: Cep.value,
+    address: Endereco.value,
+    number: Numero.value,
+    complement: Complemento.value || undefined,
+  };
 
   const headers = {
     'Content-Type': 'application/json',
-    'Authorization': `Bearer ${JSON.parse(localStorage.getItem('token'))}`
+    Authorization: `Bearer ${JSON.parse(localStorage.getItem('token'))}`,
   };
 
-  loader.classList.add('show')
-  submitText.classList.add("invisible")
-  submitButton.disabled = true
+  loader.classList.add('show');
+  submitText.classList.add('invisible');
+  submitButton.disabled = true;
 
   const requisicao = await fetch(Url, {
     method: 'POST',
@@ -123,15 +96,18 @@ async function cadastrar_endereco() {
     body: JSON.stringify(dados),
   });
 
-  loader.classList.remove('show')
-  submitText.classList.remove("invisible")
-  submitButton.disabled = false
+  loader.classList.remove('show');
+  submitText.classList.remove('invisible');
+  submitButton.disabled = false;
 
   if (requisicao.ok) {
-    alert("Endereço cadastrado com sucesso!")
+    alert('Endereço cadastrado com sucesso!');
+    window.location.href = '../view/endereco.html';
   } else {
     if (requisicao.status === 500) {
-      return alert("Erro ao realizar seu cadastro! Tente novamente mais tarde.")
+      return alert(
+        'Erro ao realizar seu cadastro! Tente novamente mais tarde.'
+      );
     }
   }
 }
